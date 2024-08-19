9a5322f693d39b0e57fd046e4fb7179fd81c9a50
manexustechnology <technology@manexus.xyz>
2024-08-19 15:04:22 +0700

    Setup Boilerplate

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,40 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
+  },
+  reactStrictMode: true,
+  // open env to client side
+  webpack: (config, { webpack, isServer }) => {
+    const envs = {};
+
+    Object.keys(process.env).forEach(env => {
+      if (env.startsWith('NEXT_PUBLIC_')) {
+        envs[env] = process.env[env];
+      }
+    })
+
+    if (!isServer) {
+      config.plugins.push(new webpack.DefinePlugin({
+        'process.env': JSON.stringify(envs),
+      }))
+    }
+
+    // https://dev.to/dinhkhai0201/module-not-found-cant-resolve-pino-pretty-g6
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push('pino-pretty', 'encoding');
+
+    return config
+  },
+};
 
 export default nextConfig;
---
+++ src/app/providers.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { chakraTheme } from '@/themes/chakra-theme'
+import { AntdRegistry } from '@ant-design/nextjs-registry'
+import { ChakraProvider } from '@chakra-ui/react'
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ChakraProvider theme={chakraTheme}>
+      <AntdRegistry>
+        {children}
+      </AntdRegistry>
+    </ChakraProvider>
+  )
+}
