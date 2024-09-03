471bda831561e88b6fc4739729e0975b619261ac
juniardys <juniardysetio@gmail.com>
2024-09-03 16:03:24 +0700

    fix: auth options

---
+++ src/config/next-auth-options.ts
@@ -0,0 +1,21 @@
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  pages: {
+    signIn: "/signin",
+  },
+  callbacks: {
+    async signIn({ account, profile }: any) {
+      if (account.provider === "google") {
+        return profile.email_verified && profile.email.endsWith("@manexus.xyz");
+      }
+      return true; // Do different verification for other providers that don't have `email_verified`
+    },
+  },
+};
