5d19af294bc9aca9150929bf444894a788fa9864
juniardys <juniardysetio@gmail.com>
2024-09-03 16:30:41 +0700

    fix: eslint error

--- src/app/(auth)/components/SignInClientPage.tsx
+++ src/app/(auth)/components/SignInClientPage.tsx
@@ -1,19 +1,33 @@
 'use client';
 
 import { Button, Card } from '@chakra-ui/react';
-import { getProviders, signIn, useSession } from 'next-auth/react';
+import { BuiltInProviderType } from 'next-auth/providers/index';
+import { ClientSafeProvider, getProviders, LiteralUnion, signIn, useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
-const SignInClientPage: React.FC = async () => {
+const SignInClientPage: React.FC = () => {
   const { data } = useSession();
+  const [providers, setProviders] = useState<Record<LiteralUnion<BuiltInProviderType, string>, ClientSafeProvider> | null>(null);
   const router = useRouter();
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      try {
+        const providers = await getProviders();
+        setProviders(providers);
+      } catch (error) {
+        console.error('Error fetching providers:', error);
+      }
+    }
+
+    fetchProviders();
+  }, []);
 
   if (data) {
     router.push('/dashboard');
     return (<></>);
   }
-
-  const providers = await getProviders();
 
   return (
     <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
