ccfa26ea51c685a6210fb88b3c42d2522d6e859f
Atharva Ashtekar <athatharva2002@gmail.com>
2024-09-16 11:12:32 +0530

    sidebar sliced

--- src/components/SidebarPanel.tsx
+++ src/components/SidebarPanel.tsx
@@ -1,8 +1,15 @@
-'use client';
+"use client";
 
 import { Sidebar, SidebarItem } from "./Sidebar";
-import { FileText } from "@phosphor-icons/react";
+import { ClockCounterClockwise, FileText } from "@phosphor-icons/react";
 import Link from "next/link";
+import {
+  Bell,
+  GearSix,
+  PresentationChart,
+  User,
+  Users,
+} from "@phosphor-icons/react/dist/ssr";
 
 interface SidebarPanel {
   isShowed?: boolean;
@@ -12,11 +19,56 @@ interface SidebarPanel {
 const SidebarPanel: React.FC<SidebarPanel> = ({ isShowed, onShow }) => {
   return (
     <Sidebar isShowed={isShowed} onShow={onShow}>
-      <Link href='/property-listing-request'>
-        <SidebarItem text="Property Listing Request" icon={<FileText size={20} weight="fill" />} />
-      </Link>
+      <div className="flex flex-col h-full">
+        {/* Sidebar items */}
+        <div className="flex flex-col flex-grow">
+          <Link href="/">
+            <SidebarItem
+              text="Dashboard"
+              icon={<PresentationChart size={20} weight="fill" />}
+            />
+          </Link>
+          <Link href="/">
+            <SidebarItem text="Listed Property" />
+          </Link>
+          <div className="flex flex-col items-start px-2.5 gap-2.5 w-full h-px">
+            <div className="w-full h-px bg-zinc-200 flex-none order-0 self-stretch flex-grow-0"></div>
+          </div>
+          <Link href="/">
+            <SidebarItem
+              text="Admin Management"
+              icon={<Users size={20} weight="fill" />}
+            />
+          </Link>
+          <Link href="/">
+            <SidebarItem
+              text="Activity Log"
+              icon={<ClockCounterClockwise size={20} weight="fill" />}
+            />
+          </Link>
+        </div>
+
+        {/* Bottom items */}
+        <div className="flex flex-col gap-2.5 px-2.5">
+          <SidebarItem
+            text="Notifications"
+            icon={<Bell size={20} weight="fill" />}
+          />
+          <SidebarItem
+            text="Settings"
+            icon={<GearSix size={20} weight="fill" />}
+          />
+          <div className="flex flex-col items-start px-2.5 gap-2.5 w-full h-px">
+            <div className="w-full h-px bg-zinc-200 flex-none order-0 self-stretch flex-grow-0"></div>
+          </div>
+          <SidebarItem
+            text="Super Admin"
+            icon={<User size={20} weight="fill" />}
+          />
+        </div>
+      </div>
     </Sidebar>
-  )
-}
+  );
+};
 
 export default SidebarPanel;
